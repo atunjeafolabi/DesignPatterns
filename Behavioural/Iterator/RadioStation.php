5998fe2d3b9fbb411fb433ff9c9116f0962967b9
Atunje <atunje_afolabi@yahoo.com>
2021-09-20 10:51:23 +0100

    implementation of iterator design pattern

---
+++ Behavioural/Iterator/RadioStation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DesignPatterns\Behavioural\Iterator;
+
+class RadioStation
+{
+    protected $frequency;
+
+    public function __construct(float $frequency)
+    {
+        $this->frequency = $frequency;
+    }
+
+    public function getFrequency(): float
+    {
+        return $this->frequency;
+    }
+}
---
+++ Behavioural/Iterator/index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DesignPatterns\Behavioural\Iterator;
+
+require_once "../../vendor/autoload.php";
+
+$stationList = new StationList();
+
+$stationList->addStation(new RadioStation(89));
+$stationList->addStation(new RadioStation(101));
+$stationList->addStation(new RadioStation(102));
+$stationList->addStation(new RadioStation(103.2));
+
+foreach($stationList as $station) {
+    echo $station->getFrequency() . PHP_EOL;
+}
+
+$stationList->removeStation(new RadioStation(89)); // Will remove station 89
